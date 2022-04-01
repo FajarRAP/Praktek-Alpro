bed2ea989ede41a9d3d2afa3e7f46e9efa63e5ce
FajarRAP <fajary781@gmail.com>
2022-04-01 17:59:49 +0700

    nambah method seleksi pengalaman & jawab

--- 2/Program/StudiKasusA.cpp
+++ 2/Program/StudiKasusA.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
 using namespace std;
 
+// bug : jika salah satu pekerjaan pengalamannya <2.5 thn
+
 class gaji{
     public:
     string nama;
     float pengalaman, gajitotal, gaji, bonus;
-    int pilih, jawab;
+    int pilih, jawab, tanda=0;
 
     int bidang(){
+        tanda++;
+        cout<<tanda<<endl;
         cout<<"Daftar bidang yang dibutuhkan : "<<endl;
         cout<<"1. Express JS\n2. Django Python\n3. Rails Ruby"<<endl;
         cout<<"4. Laravel PHP\n5. Spring Java\n6. Angular JS"<<endl;
@@ -17,6 +21,7 @@ class gaji{
     }
 
     int jawaban(){
+        cout<<endl;
         cout<<"Apakah anda ingin mengambil bidang lain?"<<endl;
         cout<<"1. Ya\n2. Tidak"<<endl;
         cout<<"Jawabanmu : ";
@@ -25,38 +30,71 @@ class gaji{
     }
 
     float experience(){
+        cout<<endl;
         cout<<"Berapa lama pengalaman anda di bidang ini (tahun) : ";
         cin>>pengalaman;
         return pengalaman;
     }
 
+    void seleksi_jawab(){
+        if(jawab==1 && tanda==2){
+            cout<<"Anda tidak dapat mengambil lagi (Maks 2)"<<endl;
+            while(jawaban()==1 || jawab>2){
+                if(jawab==2){
+                    break;
+                }
+            }
+
+            switch(jawab){
+                case 2:
+                    cout<<"Gajimu = Rp. "<<gajitotal<<" Juta"<<endl;
+                    break;
+                default:
+                    cout<<"Pilihan tidak tersedia"<<endl;
+                        
+            }
+        }else{
+            switch(jawab){
+                case 1:
+                    system("cls");
+                    bidang();
+                    pilihan();
+                    break;
+                case 2:
+                    cout<<"Gajimu = Rp. "<<gajitotal<<" Juta"<<endl;
+                    break;
+                default:
+                    cout<<"Pilihan tidak tersedia"<<endl;
+            }
+        } 
+    }
+    void seleksi_pengalaman(){
+        if(pengalaman>4){
+            bonus=gaji*0.4;
+            gaji+=bonus;
+            gajitotal+=gaji;
+            jawaban();
+            seleksi_jawab();
+        }else if(pengalaman>2.5){
+            gajitotal+=gaji;
+            jawaban();
+            seleksi_jawab();
+        }else{
+            cout<<"Gajimu = Rp. "<<gaji<<" Juta"<<endl;
+        }
+    }
     void pilihan(){
         switch(pilih){
             case 1:
                 gaji=15;
-                if(experience()>4){
-                    bonus=gaji*0.4;
-                    gaji+=bonus;
-                    gajitotal+=gaji;
-                    
-                    if(jawaban()==1){
-                        bidang();
-                        pilihan();
-                    }
-                }else if(experience()>2.5){
-                    
-                }else{
-
-                }
+                experience();
+                seleksi_pengalaman();
                 break;
             case 2:
                 gaji=13;
-                if(experience()>4){
-                    bonus=gaji*0.4;
-                    gaji+=bonus;
-                    gajitotal+=gaji;
-                    cout<<gajitotal<<endl;
-                }
+                experience();
+                seleksi_pengalaman();
+                break;
             
         }
     }
