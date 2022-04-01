c88c82dbfa63a39524edaa16fe7819da57f3d8af
FajarRAP <fajary781@gmail.com>
2022-04-01 17:07:31 +0700

    fix pemanggilan method dan gaji

--- 2/Program/StudiKasusA.cpp
+++ 2/Program/StudiKasusA.cpp
@@ -4,8 +4,8 @@ using namespace std;
 class gaji{
     public:
     string nama;
-    float pengalaman;
-    int pilih;
+    float pengalaman, gajitotal, gaji, bonus;
+    int pilih, jawab;
 
     int bidang(){
         cout<<"Daftar bidang yang dibutuhkan : "<<endl;
@@ -15,11 +15,57 @@ class gaji{
         cin>>pilih;
         return pilih;
     }
+
+    int jawaban(){
+        cout<<"Apakah anda ingin mengambil bidang lain?"<<endl;
+        cout<<"1. Ya\n2. Tidak"<<endl;
+        cout<<"Jawabanmu : ";
+        cin>>jawab;
+        return jawab;
+    }
+
+    float experience(){
+        cout<<"Berapa lama pengalaman anda di bidang ini (tahun) : ";
+        cin>>pengalaman;
+        return pengalaman;
+    }
+
+    void pilihan(){
+        switch(pilih){
+            case 1:
+                gaji=15;
+                if(experience()>4){
+                    bonus=gaji*0.4;
+                    gaji+=bonus;
+                    gajitotal+=gaji;
+                    
+                    if(jawaban()==1){
+                        bidang();
+                        pilihan();
+                    }
+                }else if(experience()>2.5){
+                    
+                }else{
+
+                }
+                break;
+            case 2:
+                gaji=13;
+                if(experience()>4){
+                    bonus=gaji*0.4;
+                    gaji+=bonus;
+                    gajitotal+=gaji;
+                    cout<<gajitotal<<endl;
+                }
+            
+        }
+    }
 };
 
 int main(){
     system("cls");
     gaji developer;
     developer.bidang();
+    developer.pilihan();
     return 0;
 }
