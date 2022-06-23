e5e8cc23db9b106a98860d1e3a0dd2818c068dbd
FajarRAP <fajary781@gmail.com>
2022-06-23 07:26:12 +0700

    modif friend

--- 9/Source Code/StudiKasus9.cpp
+++ 9/Source Code/StudiKasus9.cpp
@@ -11,19 +11,39 @@ class SK9{
         string departemenBaru[1][100], namaBaru[1][100];
         
     public:
-    void input(){
+    friend istream& operator>>(istream& titit, SK9& kelas){
         cout<<"n Data\t: ";
-        cin>>nData;
-        for(int a=0; a<nData; a++){
-            cin.ignore();
+        titit>>kelas.nData;
+        for(int a=0; a<kelas.nData; a++){
+            titit.ignore();
             cout<<"Nama\t\t: ";
-            getline(cin, nama[a]);
-            cout<<"ID\t\t\t: ";
-            cin>>idKaryawan[a];
-            cin.ignore();
+            getline(titit, kelas.nama[a]);
+            cout<<"ID\t\t: ";
+            titit>>kelas.idKaryawan[a];
+            titit.ignore();
             cout<<"Departemen\t: ";
-            getline(cin, departemen[a]);
+            getline(titit, kelas.departemen[a]);
         }
+        return titit;
+    }
+    friend ostream& operator<<(ostream& titit, SK9& kelas){
+        titit<<endl<<endl;
+        titit<<"+";
+        kelas.garis(52);
+        titit<<"+"<<endl;
+        titit<<"| "<<left<<setw(20)<<"Departemen"<<left<<setw(15)<<"ID"<<left<<setw(15)<<"Nama Karyawan   |"<<endl;
+        titit<<"+";
+        kelas.garis(52);
+        titit<<"+"<<endl;
+        for(int a=0; a<1; a++){
+            for(int b=0; b<kelas.nData; b++){
+                titit<<"| "<<left<<setw(20)<<kelas.departemenBaru[a][b]<<left<<setw(15)<<kelas.idKaryawanBaru[a][b]<<left<<setw(15)<<kelas.namaBaru[a][b]<<" |"<<endl;
+            }
+        }
+        titit<<"+";
+        kelas.garis(52);
+        titit<<"+"<<endl;
+        return titit;
     }
 
     void proses(){
@@ -36,25 +56,6 @@ class SK9{
         }
     }
 
-    void output(){
-        cout<<endl<<endl;
-        cout<<"+";
-        garis(52);
-        cout<<"+"<<endl;
-        cout<<"| "<<left<<setw(20)<<"Departemen"<<left<<setw(15)<<"ID"<<left<<setw(15)<<"Nama Karyawan   |"<<endl;
-        cout<<"+";
-        garis(52);
-        cout<<"+"<<endl;
-        for(int a=0; a<1; a++){
-            for(int b=0; b<nData; b++){
-                cout<<"| "<<left<<setw(20)<<departemenBaru[a][b]<<left<<setw(15)<<idKaryawanBaru[a][b]<<left<<setw(15)<<namaBaru[a][b]<<" |"<<endl;
-            }
-        }
-        cout<<"+";
-        garis(52);
-        cout<<"+"<<endl;
-    }
-
     void garis(int x){
         for(int a=0; a<x; a++){
             cout<<"=";
@@ -64,7 +65,7 @@ class SK9{
 
 int main() {
     SK9 gas;
-    gas.input();
+    cin>>gas;
     gas.proses();
-    gas.output();
+    cout<<gas;
 }
